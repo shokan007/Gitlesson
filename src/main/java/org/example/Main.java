804a8b570cc09bf72d05c8601d4f576d4132c442
shokan007 <bratayev89@gmail.com>
2023-05-13 20:38:36 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        mainMethodShowMessageAble();
+        mainMethodShowInfo();
+    }
+    private static void mainMethodShowMessageAble() {
+        System.out.println("Hello able QA course\n");
+        }
+    private static void mainMethodShowInfo() {
+        System.out.println("Lets learn Git");
+    }
+}
+
+
+
